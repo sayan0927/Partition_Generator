169fb86b4b92cbdd63b09ff5190aa98188b5e750
sayan <sayanpaul0927@gmail.com>
2021-12-04 22:25:36 +0530

    ver 1.0

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,12 +17,15 @@ public class Controller {
             System.out.println("\nEnter Target\n");
             int target = scanner.nextInt();
 
-            //  double start_time = new Date().getTime();
+              double start_time = new Date().getTime();
 
             Generator generator = new Generator();
             generator.compute(target);
 
             System.out.println("Total partition of the integer " + target + " is " + generator.getPartitionCount());
+
+            System.out.println("Time taken for input "+target+" is " +(new Date().getTime()-start_time)/1000+" seconds");
+
             System.out.println("Press 1 to display the partitions ");
 
             if (scanner.nextInt() == 1)
